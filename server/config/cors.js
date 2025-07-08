ef2c16a56b3f5392c262350139a1c40b544e39b0
Huỳnh Khang Vỹ <vykhanghuynh@gmail.com>
2025-07-08 09:33:21 +0700

    change cors in server

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 const corsHandler = cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://ephemeral-kelpie-56bf35.netlify.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
